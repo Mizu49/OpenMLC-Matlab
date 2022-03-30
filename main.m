d2d06a3d93b57be48f52f2578c3f2e2a22de78f3
Mizuki ABE <mizuki.abe.r4@dc.tohoku.ac.jp>
2022-03-30 15:12:37 +0900

    add files for spring-mass system

--- main.m
+++ main.m
@@ -2,10 +2,13 @@ clear
 clc
 
 addpath(genpath('MLC_tools\'))
+addpath(genpath('SpringMassSystem'))
 
 generations = 10;
 
-mlc = MLC('MLC_ex_LQR_problem')
+% mlc = MLC('MLC_ex_LQR_problem'); % demo by original developers
+mlc = MLC('MLC_springmass_problem');
+
 mlc.go(generations);
 
 mlc.show_best_indiv
